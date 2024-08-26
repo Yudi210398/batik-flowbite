600dc7debbc2070972e7c2ac0e9ba08e0d4ede04
yudirunat <yudi.berland@gmail.com>
2024-08-27 03:03:43 +0700

    dastr

--- src/app/batik/page.tsx
+++ src/app/batik/page.tsx
@@ -1,20 +1,26 @@
 import PembungkusSidebar from "../components/pembungkusSidebar";
 
-interface Batik {
+interface BatikItem {
   typeBatik: string;
   totalBatik: number;
   jenisBatik: string;
 }
 
-async function getBatikAll(): Promise<Batik> {
-  const res = await fetch(`http://localhost:3001/api/batik`, { method: "GET" });
+interface Batik {
+  result: BatikItem[];
+}
+
+async function getBatikAll(): Promise<BatikItem[]> {
+  const res = await fetch(`http://localhost:3001/api/batiks`, {
+    method: "GET",
+    next: { revalidate: 60 },
+  });
 
   return (await res).json();
 }
 
 export const BatikPage = async () => {
   const hasil = await getBatikAll();
-  console.log(hasil);
   return (
     <PembungkusSidebar>
       <div className="grid grid-cols-1 gap-4">
@@ -54,60 +60,29 @@ export const BatikPage = async () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            {hasil?.map((data, i) => (
+              <tr
+                key={i}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
               >
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  Pembelian
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">$1999</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Pembelian
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Pembelian
-                </a>
-              </td>
-            </tr>
+                  {data.typeBatik}
+                </th>
+                <td className="px-6 py-4">{data.jenisBatik}</td>
+                <td className="px-6 py-4">{data.totalBatik}</td>
+                <td className="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    Pembelian
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
